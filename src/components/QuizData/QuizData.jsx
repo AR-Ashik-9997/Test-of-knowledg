64e994f79fd5d6608e5cb2a17ddc5fe48c142ff3
AR-Ashik-9997 <ashikur.rahman9997@gmail.com>
2022-10-11 22:50:20 +0600

    complete toast notification

--- src/components/QuizData/QuizData.jsx
+++ src/components/QuizData/QuizData.jsx
@@ -1,28 +1,43 @@
-import React from "react";
-import { Col, Form } from "react-bootstrap";
+import { Col, Form, FormCheck, FormGroup } from "react-bootstrap";
 import HTMLReactParser from "html-react-parser";
+import { v4 } from "uuid";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const QuizData = ({ data }) => {
-  const { question, options } = data;
-  console.log(options);
+  const { question, options, correctAnswer } = data;
+  const handleChange = (event, correctAnswer) => {
+    const value = event.target.value;
+    return value === correctAnswer
+      ? toast.success("Correct Answer")
+      : toast.error("not write!");
+  };
   return (
     <Col lg={12} md={12} sm={12}>
       <div className="shadow p-3 bg-body rounded mt-5">
         <h3>{HTMLReactParser(question)}</h3>
-        <Form className="fs-5">
-          {["radio"].map((type) => (
-            <div key={`default-${type}`} className="mb-2">
-              {options.map((quizOption) => (
-                <Form.Check
-                  type={type}
-                  name="group1"
-                  id={`default-${type}`}
+        <Form>
+          <FormGroup>
+            {options.map((quizOption) => (
+              <div
+                className="shadow-none p-3 mb-3 bg-light rounded hover"
+                key={v4()}
+                style={{ outlineColor: "red" }}
+              >
+                <FormCheck
+                  className="fs-4"
+                  name="gender"
                   label={quizOption}
+                  value={quizOption}
+                  type="radio"
+                  onChange={(event) => handleChange(event, correctAnswer)}
                 />
-              ))}
-            </div>
-          ))}
+              </div>
+            ))}
+          </FormGroup>
         </Form>
       </div>
+      <ToastContainer position="top-center" autoClose={500} theme="dark"/>
     </Col>
   );
 };
